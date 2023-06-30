72298688552d38eea9fb9893600552cf841d69fe
Eray Demircioğlu <c.eray@hotmail.com>
2023-06-30 10:41:07 +0300

    Store

---
+++ components/ui/billboard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Billboard } from "@/types";
+
+interface BillboardProps {
+  data?: Billboard | null;
+}
+
+const Billboard: React.FC<BillboardProps> = ({ data }) => {
+  if (!data) {
+    return null; // Return null if data is undefined or null
+  }
+
+  return (
+    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+      <div
+        style={{ backgroundImage: `url(${data.imageUrl})` }}
+        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+      >
+        <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+            {data.label}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Billboard;
